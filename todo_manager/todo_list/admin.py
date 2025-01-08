9bc7ebf892ebb7c072f5fa7612e47ce910604a5b
Artem <26rus5g@gmail.com>
2025-01-08 13:09:06 +0300

    add description to admin panel

--- todo_manager/todo_list/admin.py
+++ todo_manager/todo_list/admin.py
@@ -4,5 +4,10 @@ from todo_list.models import ToDoItem
 
 @admin.register(ToDoItem)
 class ToDoItemAdmon(admin.ModelAdmin):
-    list_display = "id", "title", "done"
+    list_display = (
+        "id",
+        "title",
+        "done",
+        "description",
+    )
     list_display_links = ("id", "title")
